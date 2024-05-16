c82d6ef50bb3aa3cac3415d06a7f33dcf17ae6c8
Ranjith <ranjithp2294@gmail.com>
2024-05-16 16:06:35 +0530

    SideBar component has been created

---
+++ src/component/SideBar.jsx
@@ -0,0 +1,17 @@
+
+// eslint-disable-next-line react/prop-types
+const SideBar = ({setMenuItem,menuItem}) => {
+  return (
+    <div className="space-y-2">
+      <div className={`border rounded p-2 cursor-pointer ${menuItem === 1 ?'bg-white text-blck' : ""  }`} onClick={()=>{setMenuItem(1)}}>Dash Board</div>
+      <div className={`border rounded p-2 cursor-pointer ${menuItem === 2 ?'bg-white text-blck' : ""  }`} onClick={()=>{setMenuItem(2)}}>Add Members</div>
+      <div className={`border rounded p-2 cursor-pointer ${menuItem === 3 ?'bg-white text-blck' : ""  }`} onClick={()=>{setMenuItem(3)}}>Add Entries</div>
+      <div className={`border rounded p-2 cursor-pointer ${menuItem === 4 ?'bg-white text-blck' : ""  }`} onClick={()=>{setMenuItem(4)}}>Day Book</div>
+      <div className={`border rounded p-2 cursor-pointer ${menuItem === 5 ?'bg-white text-blck' : ""  }`} onClick={()=>{setMenuItem(5)}}>Receivebles</div>
+      <div className={`border rounded p-2 cursor-pointer ${menuItem === 6 ?'bg-white text-blck' : ""  }`} onClick={()=>{setMenuItem(6)}}>Payables</div>
+      <div className={`border rounded p-2 cursor-pointer `} >Logout</div>
+    </div>
+  )
+}
+
+export default SideBar
