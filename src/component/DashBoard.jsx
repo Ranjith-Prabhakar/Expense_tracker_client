3a26531752ccf07cea28c2b909a4a131c1e25a78
Ranjith <ranjithp2294@gmail.com>
2024-05-16 16:07:15 +0530

    DashBoard component has been created

---
+++ src/component/DashBoard.jsx
@@ -0,0 +1,16 @@
+
+const DashBoard = () => {
+  return (
+    <div className="flex flex-col w-full lg:flex-row">
+  <div className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content</div> 
+  <div className="divider lg:divider-horizontal" /> 
+  <div className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content</div>
+  <div className="divider lg:divider-horizontal" /> 
+  <div className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content</div>
+  <div className="divider lg:divider-horizontal" /> 
+  <div className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content</div>
+</div>
+  )
+}
+
+export default DashBoard
